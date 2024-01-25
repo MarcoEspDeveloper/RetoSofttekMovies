81cac9a97c1db045a44ea2760f674d109711db81
Marco Antonio Espinal <>
2024-01-25 18:20:24 -0500

    First commit

---
+++ RetoSoftekMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetoSoftekMovies
+//
+//  Created by Marco Antonio  on 25/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
